d4683318d89b50c9b25259372db5627a54848da0
aryaziz22 <e41221567@student.polije.ac.id>
2025-05-07 01:00:45 +0700

    page musik

---
+++ src/components/PopularComponentMusic.jsx
@@ -0,0 +1,38 @@
+import { Col } from "react-bootstrap";
+import { musicsTerbaru } from "../data/index";
+
+const PopularComponentMusic = () => {
+  return (
+    <>
+      {musicsTerbaru.map((musics) => {
+        return (
+          <Col lg={3} md={6} sm={12} className="music-popular-wrapper p-2">
+            <div className="music-popular-card">
+              <img
+                src={musics.image}
+                alt={musics.title}
+                className="music-popular-image"
+              />
+              <div className="music-popular-info px-3 py-2">
+                <p className="music-popular-title">{musics.title}</p>
+                <p className="music-popular-artist">{musics.artist}</p>
+                <div className="music-popular-controls d-flex justify-content-between px-1">
+                  <div class="icon-group-left">
+                    <i class="fa-solid fa-download"></i>
+                    <i class="fa-regular fa-heart"></i>
+                    <i class="fa-solid fa-ellipsis"></i>
+                  </div>
+                  <div class="icon-group-right">
+                    <i class="fa-solid fa-circle-play play-button"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+        );
+      })}
+    </>
+  );
+};
+
+export default PopularComponentMusic;
--- src/pages/MusicsPage.jsx
+++ src/pages/MusicsPage.jsx
@@ -1,12 +1,37 @@
-import { Container } from 'react-bootstrap'
 import ErrorComponents from '../components/ErrorComponents'
+import { Container, Row, Col} from "react-bootstrap";
+import CarouselComponentFilm from "../components/CarouselComponentFilm";
+import PopularComponentMusic from "../components/PopularComponentMusic";
+import KonserMusicComponent from "../components/KonserMusicComponent";
+import NewsMusicComponent from "../components/NewsMusicComponent";
 
-function TestimonialPage() {
+
+
+const MusicsPage = () => {
   return (
-    <div>
-      <ErrorComponents />
-    </div>
+      <div className='w-100 min-vh-100 homepage-musics'>
+         <div>
+            <Container >
+              <Row className="box-carousel-musics  d-flex justify-content-center">
+                <h1>Populer Musik di FindFun</h1>
+                <CarouselComponentFilm className="rounded-4" />
+              </Row>
+              <Row className="box-konser-musics p-3 m-1 mt-4" >
+                <h1 className="text-black">Konser Mendatang</h1>
+                <KonserMusicComponent />
+              </Row>
+              <Row className="box-populer-musics p-3 m-1 mt-4" >
+                <h1 className="text-black text-center">Musik Populer</h1>
+                <PopularComponentMusic />
+              </Row>
+              <Row className="box-hots-news p-3">
+                <h1 className="text-black">Berita Terbaru</h1>
+                <NewsMusicComponent />
+              </Row>
+            </Container>
+         </div>
+      </div>
   )
 }
 
-export default TestimonialPage
+export default MusicsPage
