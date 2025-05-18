caccfeec0c578b7c05862379c245a3e3861e3640
aryaziz22 <e41221567@student.polije.ac.id>
2025-05-18 20:33:57 +0700

    detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import NavbarComponent from './components/NavbarComponents';
-import FooterComponent from './components/FooterComponents';
+import NavbarComponent from "./components/NavbarComponents";
+import FooterComponent from "./components/FooterComponents";
 
-import HomePage from './pages/HomePage';
-import FilmsPage from './pages/FilmsPage';
-import FaqPage from './pages/FaqPage';
-import SyaratKetenPage from './pages/SyaratKetenPage';
-import MusicsPage from './pages/MusicsPage';
-
-
+import HomePage from "./pages/HomePage";
+import FilmsPage from "./pages/FilmsPage";
+import FaqPage from "./pages/FaqPage";
+import SyaratKetenPage from "./pages/SyaratKetenPage";
+import MusicsPage from "./pages/MusicsPage";
+import DetailMusicsPage from "./pages/DetailMusicsPage";
 
 function App() {
   return (
     <div>
       <NavbarComponent />
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/Films' element={<FilmsPage/>} />
-        <Route path='/Musics' element={<MusicsPage/>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Films" element={<FilmsPage />} />
+        <Route path="/Musics" element={<MusicsPage />} />
+        <Route path="/Musics/:id" element={<DetailMusicsPage />} />
       </Routes>
       <FooterComponent />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/PopularComponentMusic.jsx
+++ src/components/PopularComponentMusic.jsx
@@ -1,38 +1,35 @@
 import { Col } from "react-bootstrap";
-import { musicsTerbaru } from "../data/index";
+import { musicsTerbaru } from "../data";
+import { Link } from "react-router-dom";
 
 const PopularComponentMusic = () => {
   return (
     <>
-      {musicsTerbaru.map((musics) => {
-        return (
-          <Col lg={3} md={6} sm={12} className="music-popular-wrapper p-2">
+      {musicsTerbaru.map((musics) => (
+        <Col key={musics.id} lg={3} md={6} sm={12} className="music-popular-wrapper p-2">
+          <Link to={`/Musics/${musics.id}`} className="text-decoration-none text-light">
             <div className="music-popular-card">
-              <img
-                src={musics.image}
-                alt={musics.title}
-                className="music-popular-image"
-              />
+              <img src={musics.image} alt={musics.title} className="music-popular-image" />
               <div className="music-popular-info px-3 py-2">
                 <p className="music-popular-title">{musics.title}</p>
                 <p className="music-popular-artist">{musics.artist}</p>
                 <div className="music-popular-controls d-flex justify-content-between px-1">
-                  <div class="icon-group-left">
-                    <i class="fa-solid fa-download"></i>
-                    <i class="fa-regular fa-heart"></i>
-                    <i class="fa-solid fa-ellipsis"></i>
+                  <div className="icon-group-left">
+                    <i className="fa-solid fa-download"></i>
+                    <i className="fa-regular fa-heart"></i>
+                    <i className="fa-solid fa-ellipsis"></i>
                   </div>
-                  <div class="icon-group-right">
-                    <div class="play-button">
-                      <i class="fa-solid fa-play"></i>
+                  <div className="icon-group-right">
+                    <div className="play-button">
+                      <i className="fa-solid fa-play"></i>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </Col>
-        );
-      })}
+          </Link>
+        </Col>
+      ))}
     </>
   );
 };
