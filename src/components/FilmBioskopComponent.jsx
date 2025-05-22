34bee2d13f130629793a1797e809c1d694d90c7b
aryaziz22 <e41221567@student.polije.ac.id>
2025-05-22 20:25:10 +0700

    update-bioskop

---
+++ src/components/FilmBioskopComponent.jsx
@@ -0,0 +1,47 @@
+import { Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { BioskopFIlms } from "../data/index";
+
+const FilmsBioskopComponent = () => {
+  const chunkedFilms = [];
+
+  for (let i = 0; i < BioskopFIlms.length; i += 2) {
+    chunkedFilms.push(BioskopFIlms.slice(i, i + 2));
+  }
+
+  return (
+    <>
+      {chunkedFilms.map((filmPair, rowIndex) => (
+        <Row key={rowIndex} className="gx-3 mb-3 mt-3">
+          {filmPair.map((films, index) => (
+            <Col lg={6} md={12} key={films.id || index}>
+              <div className="bioskop-film-row d-flex overflow-hidden">
+                <div className="bioskop-film-img-col">
+                  <div className="bioskop-image-wrapper h-100">
+                    <img
+                      src={films.image}
+                      alt={films.title}
+                      className="img-fluid bioskop-film-image"
+                    />
+                  </div>
+                </div>
+                <div className="bioskop-film-info-col">
+                  <div className="bioskop-film-info-card d-flex flex-column justify-content-center">
+                    <span className="bioskop-film-genre">{films.genre}</span>
+                    <h3 className="bioskop-film-title">{films.title}</h3>
+                    <p className="bioskop-film-desc">{films.description}</p>
+                    <Link to={`/films/${films.id}`} className="bioskop-film-link">
+                      Lihat <span className="bioskop-arrow">›</span>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </Col>
+          ))}
+        </Row>
+      ))}
+    </>
+  );
+};
+
+export default FilmsBioskopComponent;
--- src/pages/FilmsPage.jsx
+++ src/pages/FilmsPage.jsx
@@ -1,33 +1,50 @@
-
 import ErrorComponents from "../components/ErrorComponents";
-import { Container, Row, Col} from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import CarouselComponentFilm from "../components/CarouselComponentFilm";
 import PopularComponentFilm from "../components/PopularComponentFilm";
 import NewsFimlComponen from "../components/NewsFimlComponen";
-
-
+import FilmBioskopComponent from "../components/FilmBioskopComponent"; 
 
 const FilmsPage = () => {
   return (
-      <div className='w-100 min-vh-100 homepage-films'>
-         <div>
-            <Container >
-              <Row className="box-carousel-films  d-flex justify-content-center">
-                <h1 className="mt-4"><b>Film Populer di FindFun</b></h1>
-                <CarouselComponentFilm className="rounded-4" />
-              </Row>
-              <Row className="box-populer-films p-3 m-1 mt-5" >
-                <h1><b>Film Hits</b></h1>
-                <PopularComponentFilm />
-              </Row>
-              <Row className="box-berita mt-5">
-                <h1><b>Berita hari ini</b></h1>
-                <NewsFimlComponen />
-              </Row>
-            </Container>
-         </div>
-      </div>  
-  )
-}
+    <div className="w-100 min-vh-100 homepage-films">
+      <div>
+        <Container>
+          {/* Carousel Film Populer */}
+          <Row className="box-carousel-films d-flex justify-content-center">
+            <h1 className="mt-4">
+              <b>Film Populer di FindFun</b>
+            </h1>
+            <CarouselComponentFilm className="rounded-4" />
+          </Row>
 
-export default FilmsPage
+          {/* Film Hits */}
+          <Row className="box-populer-films p-3 m-1 mt-5">
+            <h1>
+              <b>Film Hits</b>
+            </h1>
+            <PopularComponentFilm />
+          </Row>
+
+          {/* Bioskop Terbaru */}
+          <Row className="box-bioskop-terbaru p-3 m-1 mt-5">
+            <h1>
+              <b>Bioskop Terbaru</b>
+            </h1>
+            <FilmBioskopComponent />
+          </Row>
+
+          {/* Berita Hari Ini */}
+          <Row className="box-berita mt-5">
+            <h1>
+              <b>Berita hari ini</b>
+            </h1>
+            <NewsFimlComponen />
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default FilmsPage;
